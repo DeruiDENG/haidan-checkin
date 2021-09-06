96a893a6ca638e4b6f3c54d4bdf3bb41bdd56469
DeruiDENG <moonface.ddr@gmail.com>
2021-09-07 01:29:49 +0800

    Checkin implementation.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      {
-        functions: false,
-        classes: true,
-      },
-    ],
+    '@typescript-eslint/no-var-requires': 'off',
   },
   parser: '@typescript-eslint/parser',
   overrides: [
@@ -26,12 +19,6 @@ module.exports = {
       files: ['*.ts', '*.tsx'], // Your TypeScript files extension
       parserOptions: {
         project: ['./tsconfig.json'], // Specify it only for TypeScript files
-      },
-    },
-    {
-      files: ['!**/src/**'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
       },
     },
   ],
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+const checkin = async () => {
+  const username = process.env.HAIDAN_USERNAME;
+  const password = process.env.HAIDAN_PASSWORD;
+  if (!username || !password) {
+    throw new Error('Please specify username and password');
+  }
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.haidan.video/index.php');
+  await page.type('input[name="username"]', username); // Types instantly
+  await page.type('input[name="password"]', password); // Types instantly
+  await page.click('input[type="submit"]');
+
+  await page.waitForTimeout(5000);
+  await page.waitForSelector('#modalBtn123');
+  const modalBtn = await page.$('#modalBtn123');
+  const modalButtonText = await page.evaluate((el: Node) => {
+    return (el as HTMLInputElement).value;
+  }, modalBtn);
+
+  if (modalButtonText === '已经打卡') {
+    console.log('Already checked In.');
+  } else {
+    await page.click('#modalBtn');
+  }
+  await browser.close();
+};
+
+checkin();
+
+module.exports = {};
